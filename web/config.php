dabd17b96275be9dce86abcfe0239f12c911e239
weiliang1998 <43094528+weiliang1998@users.noreply.github.com>
2018-09-26 22:54:49 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,8 +1,8 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
+define('APIKEY', 'AIzaSyCWy7G0YnVJcsTVGYSazGlN5nsiZAJZGVw');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', '大爷');
 define('TITLENAME', 'DouDouTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
